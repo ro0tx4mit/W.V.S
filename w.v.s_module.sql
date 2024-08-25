fbbf826d39a7924d1ed927d54c754a1da1b0b42f
amitbisoyi <75783300+ro0tx4mit@users.noreply.github.com>
2024-08-25 08:15:37 +0530

    Create w.v.s_module.sql

---
+++ w.v.s_module.sql
@@ -0,0 +1,46 @@
++---------------------+
+|  User Input Module  |
+|---------------------|
+| - Enter URLs        |
+| - Submit URLs        |
++----------+----------+
+           |
+           v
++---------------------+
+|   Scrape Module     |
+|---------------------|
+| - HTTP Request      |
+| - Parse HTML        |
+| - Extract Data      |
+| - Handle Errors     |
++----------+----------+
+           |
+           v
++---------------------+
+|   Data Processing   |
+|---------------------|
+| - Filter Data       |
+| - Identify Critical |
+|   & High-Severity   |
+|   Vulnerabilities   |
+| - Prepare CSV       |
++----------+----------+
+           |
+           v
++---------------------+
+|    Reporting &      |
+|   Notification      |
+|---------------------|
+| - Save CSV File     |
+| - Prepare Email     |
+| - Send Email        |
++----------+----------+
+           |
+           v
++---------------------+
+|   Output & Logs     |
+|---------------------|
+| - Console Messages  |
+| - CSV Report        |
+| - Email Notifications|
++---------------------+
